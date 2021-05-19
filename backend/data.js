a4057b8e16b520fbb26c6022651cebb67fe2d3fb
Manjulajntuk <kamarajugaddamanjula@gmail.com>
2021-05-20 01:49:09 +0530

    big files

---
+++ backend/data.js
@@ -0,0 +1,100 @@
+export default {
+   products: [
+       {
+           _id: '1',
+    name: 'SLim old Shirt',
+    category: 'Shirtshj',
+    image: '/images/d1.jpg',
+    price: 60,
+    brand: 'Jee',
+    rating:4.5,
+    numReviews: 10,
+    description: 'Very good in color ',
+   
+    },
+    {
+        _id: '2',
+        name: 'SLim old Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 90,
+        brand: 'Jee',
+        rating:4.5,
+        numReviews: 10,
+        description: 'Very good in color ',
+       
+        },
+        {
+            _id: '3',
+            name: 'SLim old Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Jee',
+            rating:4.5,
+            numReviews: 10,
+            description: 'Very good in color ',
+           
+            },
+            {
+                _id: '4',
+                name: 'SLim old Phant',
+                category: 'Phant',
+                image: '/images/d1.jpg',
+                price: 60,
+                brand: 'Jee',
+                rating:4.5,
+                numReviews: 10,
+                description: 'Very good in color ',
+               
+                },
+                {
+                    _id: '5',
+                    name: 'SLim old Shirt',
+                    category: 'Shirts',
+                    image: '/images/d1.jpg',
+                    price: 60,
+                    brand: 'Jee',
+                    rating:4.5,
+                    numReviews: 10,
+                    description: 'Very good in color ',
+                   
+                    },
+                    {
+                        _id: '6',
+                        name: 'SLim old Phant',
+                        category: 'Phant',
+                        image: '/images/d1.jpg',
+                        price: 60,
+                        brand: 'Jee',
+                        rating:4.5,
+                        numReviews: 10,
+                        description: 'Very good in color ',
+                       
+                        },
+                        {
+                            _id: '7',
+                            name: 'SLim old Shirt',
+                            category: 'Shirts',
+                            image: '/images/d1.jpg',
+                            price: 60,
+                            brand: 'Jee',
+                            rating:4.5,
+                            numReviews: 10,
+                            description: 'Very good in color ',
+                           
+                            },
+                            {
+                                _id: '8',
+                                name: 'SLim old Phant',
+                                category: 'Pants',
+                                image: '/images/d1.jpg',
+                                price: 60,
+                                brand: 'Jee',
+                                rating:4.5,
+                                numReviews: 10,
+                                description: 'Very good in color ',
+                               
+                                },
+]
+}
